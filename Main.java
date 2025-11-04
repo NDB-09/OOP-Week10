11d7941ca1c1bdd6f9ef8af061836b6fe33e6c02
Bính <ndb09022006@gmail.com>
2025-11-04 10:51:15 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Student s1 = new Student("Nguyen Van A", "a@gmail.com", "S001");
         Student s2 = new Student("Le Thi B", "b@gmail.com", "S002");
-        Teacher t1 = new Teacher("Pham Tien Dung", "dung@uni.edu", "T01", "Dr.");
-        Advisor a1 = new Advisor("Doan Quoc Khanh", "khanh@company.com");
+        Teacher t1 = new Teacher("Pham Tien D", "d@uni.edu", "T01", "Dr.");
+        Advisor a1 = new Advisor("Doan Quoc K", "k@company.com");
 
         ListKLTN<KLTN<Student, Person>> list = new ListKLTN<>();
         list.add(new KLTN<>(s1, t1, "AI-based System"));
-        list.add(new KLTN<>(s2, a1, "Blockchain Analysis"));
+        list.add(new KLTN<>(s2, a1, "Hack all system"));
 
         System.out.println("=== Danh sách khóa luận (sắp xếp theo tên sinh viên) ===");
         list.print();
